cd0f9f80fc3b5ec528dbd91d1a8699a3e1f1b266
Milena <chudakovamilena587@gmail.com>
2022-11-06 00:35:01 +0500

    Task done

--- conftest.py
+++ conftest.py
@@ -1,12 +1,14 @@
+import time
+
 import pytest
-from src.Triangle import Triangle
-from src.Square import Square
-from src.Circle import Circle
-from src.Rectangle import Rectangle
-import requests
+from task2.Triangle import Triangle
+from task2.Square import Square
+from task2.Circle import Circle
+from task2.Rectangle import Rectangle
 import os
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.safari.options import Options
 
 
 @pytest.fixture
@@ -58,7 +60,10 @@ def pytest_addoption(parser):
     )
     parser.addoption("--url", action="store", default="http://192.168.0.8:8081", help="reference on the web-site"
                      )
-    parser.addoption("--drivers", action="store_true", default=os.path.expanduser("~/Desktop/Drivers"), help="path to drivers")
+    parser.addoption("--drivers", action="store_true", default=os.path.expanduser("~/Desktop/Drivers"),
+                     help="path to drivers")
+    parser.addoption("--executor", action="store", default="192.168.0.8", help="The web page of selenoid")
+    parser.addoption("--bv")
 
 
 @pytest.fixture
@@ -69,15 +74,50 @@ def base_url(request):
 @pytest.fixture
 def run_browser(request):
     browser = request.config.getoption("--browser")
+    executor = request.config.getoption("--executor")
+    browser_version = request.config.getoption("--bv")
     drivers_folder = request.config.getoption("--drivers")
-    if browser == "chrome":
-        driver = webdriver.Chrome(executable_path=f"{drivers_folder}/chromedriver.exe")
+
+    executor_url = "http://{}:4444/wd/hub".format(executor)
+
+    caps = {
+        "browserName": browser,
+        "platformname": "WINDOWS",
+        #"browserVersion": browser_version,
+        #"screenResolution": "1280x720",
+    }
+
+    options = Options()
+
+    driver = webdriver.Remote(
+        command_executor=executor_url,
+        desired_capabilities=caps,
+        options=options
+    )
+
+    failed_before = request.session.testsfailed
+
+    yield driver
+
+    if request.session.testsfailed != failed_before:
+        test_name = request.node.name
+    take_screenshot(driver, test_name)
+
+    driver.close()
+
+
+
+"""
+    driver = webdriver.Chrome(executable_path=f"{drivers_folder}/chromedriver.exe")
     elif browser == "firefox":
         driver = webdriver.Firefox(executable_path=f"{drivers_folder}/geckodriver.exe")
     elif browser == "yandex":
         driver = webdriver.Chrome(executable_path=f"{drivers_folder}/yandexdriver.exe")
     else:
         raise ValueError("This browser doesn't exists")
+"""
 
-    yield driver
-    driver.close()
+
+def take_screenshot(driver, test_name):
+    screenshot_file_path = "{}/{}.png".format("tests/logs/", test_name)
+    driver.save_screenshot(screenshot_file_path)
