dac92dff9eb3fe652eef74b21bfeef1a3177f323
Milena <chudakovamilena587@gmail.com>
2022-12-17 01:11:52 +0500

    Changed conftest

--- conftest.py
+++ conftest.py
@@ -1,40 +1,9 @@
 import time
-
-import pytest
-from task2.Triangle import Triangle
-from task2.Square import Square
-from task2.Circle import Circle
-from task2.Rectangle import Rectangle
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-
 # from selenium.webdriver.safari.options import Options
-
-
-@pytest.fixture
-def area_tr(a, b, c):
-    triangle = Triangle(a, b, c)
-    return triangle.area()
-
-
-@pytest.fixture
-def area_sq(a):
-    square = Square(a)
-    return square.area()
-
-
-@pytest.fixture
-def area_ci(a):
-    circle = Circle(a)
-    return circle.area()
-
-
-@pytest.fixture
-def area_re(a, b):
-    rectangle = Rectangle(a, b)
-    return rectangle.area()
+import pytest
 
 
 """
@@ -85,7 +54,7 @@ def run_browser(request):
     caps = {
         "browserName": browser,
         "platformname": "WINDOWS",
-        # "browserVersion": browser_version,
+        "browserVersion": browser_version,
         # "screenResolution": "1280x720",
     }
 
